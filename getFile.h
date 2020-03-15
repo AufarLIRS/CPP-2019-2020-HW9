f6cefc47d24713c4d675c411be4a20ddd5efb81b
Artyom <smart.artyom@gmail.com>
2020-03-15 14:05:25 +0300

    HW by Artyom Ivanov

---
+++ getFile.h
@@ -0,0 +1,43 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <QFileDialog>
+#include <QDebug>
+
+class IPrintable
+{
+public:
+  virtual ~IPrintable() = default;
+  virtual QString print() = 0;
+};
+
+class IAvailablePath
+{
+public:
+  virtual ~IAvailablePath() = default;
+  virtual void setPath(QString path) = 0;
+};
+
+class IPrintablePath : public IPrintable, public IAvailablePath
+{
+};
+
+class getFile : public IPrintablePath
+{
+  QFile file_;
+
+public:
+  void setPath(QString path) override;
+  QString print() override;
+};
+
+class Folder : public IPrintablePath
+{
+  QDir directory_;
+
+public:
+  void setPath(QString path) override;
+  QString print() override;
+};
+
+#endif  // FILE_H
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <memory>
+#include "getFile.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
+
+private slots:
+  void on_selectFilePushButton_clicked();
+
+  void on_selectFolderPushButton_clicked();
+
+  void on_Print_clicked();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow* ui;
+  std::unique_ptr<IPrintablePath> ptr;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
